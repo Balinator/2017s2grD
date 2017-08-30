23d24238c5d8f9578cb5cba7da5c542071f2675a
Fulop Szabolcs <fulopszabolcs94@gmail.com>
2017-08-30 17:35:04 +0300

    working version 1

---
+++ jbugs-persistence/src/main/java/entities/User.java
@@ -0,0 +1,128 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name = "listAllUser", query = "select u from User as u")
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String toString() {
+		return "User [idUser=" + idUser + ", firstName=" + firstName + ", lastName=" + lastName + ", userName="
+				+ userName + ", email=" + email + ", password=" + password + ", phoneNumber=" + phoneNumber
+				+ ", status=" + status + ", roles=" + roles + "]";
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idUser;
+	private String firstName;
+	private String lastName;
+	@Column(length = 6, unique = true)
+	private String userName;
+	private String email;
+	private String password;
+	@Column(length = 14)
+	private String phoneNumber;
+	private boolean status;
+
+	@ManyToMany
+	@JoinTable(name = "User_Role", joinColumns = @JoinColumn(name = "idUser"), inverseJoinColumns = {
+			@JoinColumn(name = "idRole") })
+
+	private List<Role> roles;
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public User() {
+		super();
+	}
+
+	public User(String firstName, String lastName, String userName, String email, String password, String phoneNumber,
+			boolean status) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+		this.status = status;
+	}
+
+}
---
+++ jbugs-persistence/src/main/java/operations/UserDAOImpl.java
@@ -0,0 +1,46 @@
+package operations;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import entities.User;
+
+@Stateless(name = "UserDAOImpl", mappedName = "UserDAOImpl")
+public class UserDAOImpl implements UserDAO {
+
+	@PersistenceContext(unitName = "jbugs-persistence")
+	private EntityManager em;
+
+	@Override
+	public void persistUser(User user) {
+		em.persist(user);
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		em.remove(user);
+	}
+
+	@Override
+	public void updateUser(User user) {
+		em.merge(user);
+	}
+
+	@Override
+	public User findUser(int userId) {
+		User user = em.find(User.class, userId);
+		return user;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<User> listAllUsers() {
+		List<User> users;
+		users = em.createNamedQuery("listAllUser").getResultList();
+		return users;
+	}
+
+}
