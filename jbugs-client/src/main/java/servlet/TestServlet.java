f007a2993439e4b91c90607f7cae25be1a0ef27f
FirstName LastName <yourmail@gmail.com>
2017-08-31 16:14:32 +0300

    update

--- jbugs-client/src/main/java/servlet/TestServlet.java
+++ jbugs-client/src/main/java/servlet/TestServlet.java
@@ -1,15 +1,22 @@
+package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import msg.ejb.SomeEjb;
+
 @WebServlet(urlPatterns = { "/TestServlet" })
 public class TestServlet extends HttpServlet {
+
+	@EJB
+	private SomeEjb someEjbBean;
 
 	/**
 	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -24,17 +31,19 @@ public class TestServlet extends HttpServlet {
 	 * @throws IOException
 	 *             if an I/O error occurs
 	 */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+	protected void processRequest(final HttpServletRequest request, final HttpServletResponse response)
 			throws ServletException, IOException {
+
 		response.setContentType("text/html;charset=UTF-8");
 		try (PrintWriter out = response.getWriter()) {
 			out.println("<!DOCTYPE html>");
 			out.println("<html>");
 			out.println("<head>");
-			out.println("<title>Test EJB Bean</title>");
+			out.println("<title>Test EJB Bean New</title>");
 			out.println("</head>");
 			out.println("<body>");
-			out.println("Hello world! <br>");
+			out.println(someEjbBean.hello() + "<br>");
+
 			out.println("</body>");
 			out.println("</html>");
 		}
@@ -55,7 +64,7 @@ public class TestServlet extends HttpServlet {
 	 *             if an I/O error occurs
 	 */
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
 			throws ServletException, IOException {
 		processRequest(request, response);
 	}
@@ -73,7 +82,7 @@ public class TestServlet extends HttpServlet {
 	 *             if an I/O error occurs
 	 */
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
 			throws ServletException, IOException {
 		processRequest(request, response);
 	}
