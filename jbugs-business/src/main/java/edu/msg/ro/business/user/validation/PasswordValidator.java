866685b4bcc6d3d2e5ffc1bb054c2c76dba966f3
Nagy Attila <nattila994@gmail.com>
2017-09-11 17:45:13 +0300

    Working version

--- jbugs-business/src/main/java/edu/msg/ro/business/user/validation/PasswordValidator.java
+++ jbugs-business/src/main/java/edu/msg/ro/business/user/validation/PasswordValidator.java
@@ -8,7 +8,7 @@ import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
 /**
- * @author balinc
+ * @author nagya
  *
  */
 @FacesValidator("passwordValidator")
--- jbugs-client/src/main/java/edu/msg/ro/bean/UserBean.java
+++ jbugs-client/src/main/java/edu/msg/ro/bean/UserBean.java
@@ -3,37 +3,39 @@ package edu.msg.ro.bean;
 import java.util.List;
 
 import javax.ejb.EJB;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.faces.context.FacesContext;
+import javax.faces.bean.SessionScoped;
 
 import edu.msg.ro.business.common.exception.BusinessException;
+import edu.msg.ro.business.common.exception.JBugsExeption;
 import edu.msg.ro.business.user.boundary.UserFacade;
 import edu.msg.ro.business.user.dto.UserDTO;
 
 @ManagedBean
-@RequestScoped
-public class UserBean {
+@SessionScoped
+public class UserBean extends AbstractBean {
 
 	@EJB
 	UserFacade userFacade;
 
 	private UserDTO newUser = new UserDTO();
 
-	/**
-	 * @return the user
-	 */
+	private UserDTO selectedUser = new UserDTO();
+
 	public UserDTO getNewUser() {
 		return newUser;
 	}
 
-	/**
-	 * @param user
-	 *            the user to set
-	 */
 	public void setNewUser(UserDTO user) {
 		this.newUser = user;
+	}
+
+	public UserDTO getSelectedUser() {
+		return selectedUser;
+	}
+
+	public void setSelectedUser(UserDTO selectedUser) {
+		this.selectedUser = selectedUser;
 	}
 
 	public List<UserDTO> getAllUsers() {
@@ -43,18 +45,43 @@ public class UserBean {
 
 	public String createNewUser() throws BusinessException {
 		userFacade.createUser(newUser);
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage("Userul " + newUser.getFirstname() + " a fost creat!"));
+		addMessage("Userul " + newUser.getFirstname() + " a fost creat!");
 		newUser = new UserDTO();
 		return "users";
 	}
 
 	public String deleteUser(UserDTO user) {
-		userFacade.deleteUser(user);
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage("Userul " + newUser.getFirstname() + " a fost sters!"));
+		try {
+			userFacade.deleteUser(user);
+			addMessage("Userul " + newUser.getFirstname() + " a fost sters!");
+		} catch (JBugsExeption e) {
+			handleExeptionI18n(e);
+		}
 		return "users";
+	}
 
+	public String enterUpdateMode(UserDTO user) {
+		this.selectedUser = user;
+		return "users";
+	}
+
+	public String leaveUpdateMode() {
+		selectedUser = new UserDTO();
+		return "users";
+	}
+
+	public boolean verifyUserRendere(UserDTO user) {
+		return selectedUser != null && user.getId().equals(selectedUser.getId());
+	}
+
+	public String editUser() {
+		try {
+			userFacade.updateUser(selectedUser);
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+		selectedUser = new UserDTO();
+		return "users";
 	}
 
 }
