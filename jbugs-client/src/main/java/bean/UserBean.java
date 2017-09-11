122a2c611d498e26def19edab718121988437a30
Varadi Peter <varadipeter9@gmail.com>
2017-09-11 15:59:00 +0300

    update for user inferface

--- jbugs-client/src/main/java/bean/UserBean.java
+++ jbugs-client/src/main/java/bean/UserBean.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import edu.msg.ro.business.common.exception.BusinessException;
@@ -13,10 +13,12 @@ import edu.msg.ro.business.user.boundary.UserFacade;
 import edu.msg.ro.business.user.dto.UserDTO;
 
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class UserBean {
 	@EJB
 	UserFacade userFacade;
+
+	private UserDTO selectedUser = new UserDTO();
 
 	private UserDTO newUser = new UserDTO();
 
@@ -31,8 +33,17 @@ public class UserBean {
 	 * @param user
 	 *            the user to set
 	 */
+
 	public void setNewUser(UserDTO user) {
 		this.newUser = user;
+	}
+
+	public UserDTO getSelectedUser() {
+		return selectedUser;
+	}
+
+	public void setSelectedUser(UserDTO selectedUser) {
+		this.selectedUser = selectedUser;
 	}
 
 	public List<UserDTO> getAllUsers() {
@@ -56,4 +67,28 @@ public class UserBean {
 
 	}
 
+	public String enterUpdateMode(UserDTO user) {
+		this.selectedUser = user;
+		return "users";
+	}
+
+	public String leaveUpdateMode() {
+		selectedUser = new UserDTO();
+		return "users";
+	}
+
+	public boolean verifyUserRendered(UserDTO user) {
+
+		return selectedUser != null && user.getId().equals(selectedUser.getId());
+	}
+
+	public String editUser() {
+		try {
+			userFacade.updateUser(selectedUser);
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+		selectedUser = new UserDTO();
+		return "users";
+	}
 }
--- jbugs-persistence/src/main/resources/META-INF/loadTestdata.sql
+++ jbugs-persistence/src/main/resources/META-INF/loadTestdata.sql
@@ -1,4 +1,5 @@
 -- User test data
 insert into msg_training.user(active,email,firstname,lastname,username,password,lockversion) value(true,'email','firstanem','lastname','test','test',12);
+insert into msg_training.user(active,email,firstname,lastname,username,password,lockversion) value(false,'email2','firstanem2','lastname2','test2','test2',152);
 
 -- other tables TODO
