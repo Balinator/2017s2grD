aa751131c0d3dbd343b9ca48c2ffeaed085a87f6
FirstName LastName <yourmail@gmail.com>
2017-09-19 16:27:56 +0300

    remove unused functionality from user update bean

--- jbugs-client/src/main/java/edu/msg/ro/bean/user/UserUpdateBean.java
+++ jbugs-client/src/main/java/edu/msg/ro/bean/user/UserUpdateBean.java
@@ -1,14 +1,10 @@
 package edu.msg.ro.bean.user;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import edu.msg.ro.business.common.exception.JBugsExeption;
 import edu.msg.ro.business.common.exception.TechnicalExeption;
-import edu.msg.ro.business.user.dto.RoleDTO;
 import edu.msg.ro.business.user.dto.UserDTO;
 import edu.msg.ro.persistence.user.entity.User;
 
@@ -23,8 +19,6 @@ import edu.msg.ro.persistence.user.entity.User;
 public class UserUpdateBean extends AbstractUserBean {
 
 	private UserDTO updatedUser = new UserDTO();
-
-	private List<RoleDTO> selectedRoles = new ArrayList<RoleDTO>();
 
 	/**
 	 * Get user to update.
@@ -42,26 +36,7 @@ public class UserUpdateBean extends AbstractUserBean {
 	 */
 	public void setUpdatedUser(UserDTO userDTO) {
 		rebuildRoleService();
-		setSelectedRoles(userDTO.getRoles());
 		this.updatedUser = userDTO;
-	}
-
-	/**
-	 * Get selected roles for the current edited {@link User}
-	 * 
-	 * @return
-	 */
-	public List<RoleDTO> getSelectedRoles() {
-		return selectedRoles;
-	}
-
-	/**
-	 * Set selected roles for the current edited {@link User}
-	 * 
-	 * @param selectedRoles
-	 */
-	public void setSelectedRoles(List<RoleDTO> selectedRoles) {
-		this.selectedRoles = selectedRoles;
 	}
 
 	/**
