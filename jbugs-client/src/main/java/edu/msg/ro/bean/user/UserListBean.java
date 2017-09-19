dd7f236d3ca04ff585ca9ca2f7ae3e85f8daa519
Attila Nemeth <nemethati10@gmail.com>
2017-09-19 10:48:04 +0300

    added UserListBean & refactored users.xhtml

---
+++ jbugs-client/src/main/java/edu/msg/ro/bean/user/UserListBean.java
@@ -0,0 +1,45 @@
+package edu.msg.ro.bean.user;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import edu.msg.ro.business.user.dto.UserDTO;
+import edu.msg.ro.persistence.user.entity.User;
+
+@ManagedBean
+@RequestScoped
+public class UserListBean extends AbstractUserBean {
+
+	private UserDTO userDTO = new UserDTO();
+
+	/**
+	 * Method for verifying if element needed to render.
+	 * 
+	 * @param user
+	 * @return
+	 */
+	public boolean verifyUserRendere(UserDTO user) {
+		return userDTO != null && user.getId().equals(userDTO.getId());
+	}
+
+	/**
+	 * get for selectedUser.
+	 * 
+	 * @return
+	 */
+	public UserDTO getSelectedUser() {
+		return userDTO;
+	}
+
+	/**
+	 * Method for get all {@link User}s.
+	 * 
+	 * @return
+	 */
+	public List<UserDTO> getAllUsers() {
+		return userFacade.getAllUsers();
+	}
+
+}
