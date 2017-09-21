a5c6f4eb26d847564b91eec7385d925afb46c41d
Nagy Attila <nattila994@gmail.com>
2017-09-21 13:05:54 +0300

    commit because of pull

--- jbugs-client/src/main/java/edu/msg/ro/bean/LoginBean.java
+++ jbugs-client/src/main/java/edu/msg/ro/bean/LoginBean.java
@@ -66,24 +66,31 @@ public class LoginBean extends AbstractBean implements Serializable {
 	 * @return
 	 */
 	public String processLogin() {
+		HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
 		if (loginFacade.isValidUser(user)) {
-			HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
 			session.setAttribute("username", user.getUsername());
 			addI18nMessage("login.welcome");
 			FAILEDATTEMPS = 0;
+			session.setAttribute("FAILEDATTEMPS", FAILEDATTEMPS);
 			return "bugManagment";
 		} else {
 			String loggingUser = user.getUsername();
-			if (loggingUser.equals(OLDUSERNAME)) {
-				FAILEDATTEMPS++;
-				if (FAILEDATTEMPS >= 5) {
+			if (loggingUser.equals(session.getAttribute("OLDUSERNAME"))) {
+
+				int x = Integer.parseInt(session.getAttribute("FAILEDATTEMPS").toString());
+				x++;
+				session.setAttribute("FAILEDATTEMPS", x);
+				if (x >= 4) {
 					userFacade.deleteUserNoCheck(user);
 					addI18nMessage("login.wrongpassword");
 					FAILEDATTEMPS = 0;
+					session.setAttribute("FAILEDATTEMPS", FAILEDATTEMPS);
 				}
 			} else {
 				FAILEDATTEMPS = 0;
+				session.setAttribute("FAILEDATTEMPS", FAILEDATTEMPS);
 				OLDUSERNAME = user.getUsername();
+				session.setAttribute("OLDUSERNAME", OLDUSERNAME);
 			}
 			addI18nMessage("loginForm:username", "login.error");
 			return LOGIN;
