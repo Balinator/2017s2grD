48cbcc5cadf7b1f726005822f30a83cb7ff66fd2
Varadi Peter <varadipeter9@gmail.com>
2017-09-25 18:11:47 +0300

    class for all unit tests

---
+++ jbugs-business/src/test/java/edu/msg/ro/business/allunit/Alltest.java
@@ -0,0 +1,22 @@
+package edu.msg.ro.business.allunit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import edu.msg.ro.business.junit.bug.bundary.BugFacadeTest;
+import edu.msg.ro.business.junit.bug.control.BugServiceTest;
+import edu.msg.ro.business.junit.user.boundary.RoleFacadeTest;
+import edu.msg.ro.business.junit.user.boundary.UserFacadeTest;
+import edu.msg.ro.business.junit.user.boundary.UserLoginTest;
+import edu.msg.ro.business.junit.user.control.PermissionServiceTest;
+import edu.msg.ro.business.junit.user.control.RoleServiceTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BugFacadeTest.class, BugServiceTest.class, RoleFacadeTest.class, UserFacadeTest.class,
+		PermissionServiceTest.class, UserLoginTest.class, RoleServiceTest.class
+
+})
+public class Alltest {
+
+}
