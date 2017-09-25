d9d0e8b6818eb1fc676c96264ca192630669bc7e
Fulop Szabolcs <fulopszabolcs94@gmail.com>
2017-09-25 15:12:16 +0300

    first Junit test

---
+++ jbugs-business/src/test/java/edu/msg/ro/business/junit/user/boundary/UserLoginTest.java
@@ -0,0 +1,44 @@
+package edu.msg.ro.business.junit.user.boundary;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import edu.msg.ro.business.common.exception.BusinessException;
+import edu.msg.ro.business.user.boundary.LoginFacade;
+import edu.msg.ro.business.user.control.UserService;
+import edu.msg.ro.business.user.dto.UserDTO;
+import edu.msg.ro.business.user.util.UserGenerator;
+import edu.msg.ro.business.util.TestHelper;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserLoginTest {
+
+	@InjectMocks
+	LoginFacade loginFacade;
+
+	@Mock
+	UserGenerator userPass;
+
+	@Mock
+	UserService userService;
+
+	@Test
+	public void testIsValidUser() throws BusinessException {
+		TestHelper th = new TestHelper();
+		UserDTO toTest = th.initializUser("firstname", "lastname", "email@msggroup.com", "password", "0748102601");
+
+		boolean result = loginFacade.isValidUser(toTest);
+
+		verify(userPass, times(1)).encryptPassword(toTest);
+		verify(userService, times(1)).findUserExists(eq(toTest.getUsername()), any(String.class));
+	}
+
+}
