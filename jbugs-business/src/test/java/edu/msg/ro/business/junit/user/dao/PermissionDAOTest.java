401c28f4f3a58948f68ef0f227c6c05f1c039d1c
Attila Nemeth <nemethati10@gmail.com>
2017-09-25 16:42:58 +0300

    refactored tests

--- jbugs-business/src/test/java/edu/msg/ro/business/junit/user/dao/PermissionDAOTest.java
+++
@@ -1,9 +0,0 @@
-package edu.msg.ro.business.junit.user.dao;
-
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public enum PermissionDAOTest {
-
-}
