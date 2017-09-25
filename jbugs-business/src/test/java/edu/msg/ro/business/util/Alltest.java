70c9acc231b18ef8a917ada106a01cf91464c535
Varadi Peter <varadipeter9@gmail.com>
2017-09-25 20:48:27 +0300

    test for UserValidation

--- jbugs-business/src/test/java/edu/msg/ro/business/util/Alltest.java
+++ jbugs-business/src/test/java/edu/msg/ro/business/util/Alltest.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import edu.msg.ro.business.integration.user.validator.UserValidatorTest;
 import edu.msg.ro.business.junit.bug.bundary.BugFacadeTest;
 import edu.msg.ro.business.junit.bug.control.BugServiceTest;
 import edu.msg.ro.business.junit.bug.dto.mapper.BugDTOMapperTest;
@@ -24,7 +25,7 @@ import edu.msg.ro.business.junit.user.dto.mapper.UserDTOMapperTest;
 @RunWith(Suite.class)
 @SuiteClasses({ BugFacadeTest.class, BugServiceTest.class, RoleFacadeTest.class, UserFacadeTest.class,
 		UserLoginTest.class, UserPermissonTest.class, PermissionServiceTest.class, RoleServiceTest.class,
-		UserDTOMapperTest.class, BugDTOMapperTest.class
+		UserDTOMapperTest.class, BugDTOMapperTest.class, UserValidatorTest.class
 
 })
 public class Alltest {
