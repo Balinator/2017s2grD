faaf89e5aca255c53a0e2d6bfc84e6b4f7a5a8f1
Laszlo Balazs <laszlo_balazs2007@yahoo.com>
2017-10-05 17:24:25 +0300

    notification click problem solved

--- jbugs-client/src/main/webapp/resources/js/notification.js
+++ jbugs-client/src/main/webapp/resources/js/notification.js
@@ -1,8 +1,15 @@
 function dropDown() {
 	var myDropdown = document.getElementById("notificationForm:notificationContent");
+    if (!myDropdown.classList.contains('show')) {
+    	myDropdown.classList.toggle('show');
+    }
+}
+
+function dropUp() {
+	var myDropdown = document.getElementById("notificationForm:notificationContent");
     if (myDropdown.classList.contains('show')) {
     	myDropdown.classList.remove('show');
     }else{
-    	myDropdown.classList.toggle('show');
+    	rc();
     }
 }
